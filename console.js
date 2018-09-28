f5c52e6c5a1e48d57b5c9942676ff798e67fe3bc
Abdulwahid Gul <abdulwahidgul@gmail.com>
2018-09-28 18:05:31 +0500

    balance style change, asset payment still issue

--- console.js
+++ console.js
@@ -14,60 +14,60 @@ let amount = process.argv[6];
 var distributor = stellarSdk.Keypair.fromSecret(distSecret);
 var issuer = stellarSdk.Keypair.fromSecret(issuerSceret);
 
-horizon.loadAccount(distributor.publicKey())
-    .then(function(dist){
-        var Asset = new stellarSdk.Asset(newAsset, issuer.publicKey());  
-        console.log(Asset);
+// horizon.loadAccount(distributor.publicKey())
+//     .then(function(dist){
+//         var Asset = new stellarSdk.Asset(newAsset, issuer.publicKey());  
+//         console.log(Asset);
               
-        var transaction = new stellarSdk.TransactionBuilder(dist)
-            .addOperation(stellarSdk.Operation.changeTrust({ 
-                asset: Asset,
-                limit: limit
-            }))
-            .build();
+//         var transaction = new stellarSdk.TransactionBuilder(dist)
+//             .addOperation(stellarSdk.Operation.changeTrust({ 
+//                 asset: Asset,
+//                 limit: limit
+//             }))
+//             .build();
         
-        transaction.sign(distributor);
-        return horizon.submitTransaction(transaction);
-        // horizon.submitTransaction(transaction).then(function(result) {
-        //     console.log('Change Trust Sucessful');
-        //     // console.log(result);
-        // }).catch(function(error) {
-        //     console.log('Change trust Failed!');
-        //     console.log(error);
+//         transaction.sign(distributor);
+//         return horizon.submitTransaction(transaction);
+//         // horizon.submitTransaction(transaction).then(function(result) {
+//         //     console.log('Change Trust Sucessful');
+//         //     // console.log(result);
+//         // }).catch(function(error) {
+//         //     console.log('Change trust Failed!');
+//         //     console.log(error);
             
-        // })
-    })
-    // .then(function(){
-    //     return horizon.loadAccount(issuer.publicKey())
-    // })
-    // .then(function(issue) {
-    //     var Asset = new stellarSdk.Asset(newAsset, issuer.publicKey());
-    //     var transaction = new stellarSdk.TransactionBuilder(issue)
-    //         .addOperation(stellarSdk.Operation.payment({
-    //             destination: distributor.publicKey(),
-    //             asset: Asset,
-    //             amount: '10000000'
-    //         }))
-    //         .build();
+//         // })
+//     })
+//     // .then(function(){
+//     //     return horizon.loadAccount(issuer.publicKey())
+//     // })
+//     // .then(function(issue) {
+//     //     var Asset = new stellarSdk.Asset(newAsset, issuer.publicKey());
+//     //     var transaction = new stellarSdk.TransactionBuilder(issue)
+//     //         .addOperation(stellarSdk.Operation.payment({
+//     //             destination: distributor.publicKey(),
+//     //             asset: Asset,
+//     //             amount: '10000000'
+//     //         }))
+//     //         .build();
 
-    //         transaction.sign(issuer);
-    //         horizon.submitTransaction(transaction).then(function(results){
-    //             console.log('Asset Distribution Sucessful');
-    //             // console.log(results);
-    //         }).catch(function(err){
-    //             console.log('Asset Distribution Failer');
-    //             // console.log(err);
+//     //         transaction.sign(issuer);
+//     //         horizon.submitTransaction(transaction).then(function(results){
+//     //             console.log('Asset Distribution Sucessful');
+//     //             // console.log(results);
+//     //         }).catch(function(err){
+//     //             console.log('Asset Distribution Failer');
+//     //             // console.log(err);
                 
                 
-    //         })
-    // })
-    // .then(function () {
-    //     horizon.loadAccount(distributor.publicKey())
-    //         .then(function (dist) {
-    //             // console.log(dist.balances);
+//     //         })
+//     // })
+//     // .then(function () {
+//     //     horizon.loadAccount(distributor.publicKey())
+//     //         .then(function (dist) {
+//     //             // console.log(dist.balances);
 
-    //         })
-    // })
-    .catch(function(error){
-        console.error(error);
-    })
+//     //         })
+//     // })
+//     .catch(function(error){
+//         console.error(error);
+//     })
--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.post('/wallets', async function (req, res) {
                 tx_xdr: body
             });
         } else {
-            let amount = await getBalance('GACPFYOYFARJAZ3QRAYBTZYTNWNSCVDVBFG7DHVJDBR7OQJZWPLLKEQY');
+            let amount = await getBalance(wallet.publicKey());
 
             res.render('create_wallet', {
                 publicKey: wallet.publicKey(),
@@ -74,7 +74,7 @@ app.post('/wallets', async function (req, res) {
     // return null;
 });
 app.get('/transactions', async function (req, res) {
-    var txns = {};
+    var txns = null;
     if (req.query.txn) {
         txns = await horizon.transactions().forAccount(req.query.txn).call();
     }
@@ -149,54 +149,116 @@ app.get('/assets', function(req, res) {
 });
 
 app.post('/assets', async function(req, res) {
-    var error = null;
-    // console.log(req.body);
+    let error = null;
+    // let distributor = stellarSdk.Keypair.fromSecret(req.body.receiverSceret);
+    // let issuer = stellarSdk.Keypair.fromSecret(req.body.issuerSecret);
+    // let limit = req.body.limit;
+    // let amount = req.body.amount;
+    // let assetName = req.body.assetName;
+    // let newAssets = new stellarSdk.Asset(assetName, issuer.publicKey());
+    let distributor = stellarSdk.Keypair.fromSecret('SBSN5GRJERHJBWJFETRZXQL6HEG2RMOQOJ7M77BFFIGSVJJ24PE5OY2G');
+    let issuer = stellarSdk.Keypair.fromSecret('SCADDLLPJHHFXWUMQLNBB7WHDJQMFJKOZ2N2JM6H4CK3255PTNL4G7AB');
+    // let limit = '10000000.0000000';
+    // let amount = '10000000.0000000';
+    // let assetName = 'AWG';
+    let newAssets = new stellarSdk.Asset('AWG', issuer.publicKey());
     
-    await horizon.loadAccount(req.body.receiverPublicKey)
-        .then(async function (dist) {
-            var newAsset = new stellarSdk.Asset(req.body.assetName, req.body.issuerPublicKey);
-            var transaction = new stellarSdk.TransactionBuilder(dist)
-                .addOperation(stellarSdk.Operation.changeTrust({
-                    asset: newAsset,
-                    limit: req.body.limit
-                }))
-                .build();
-
-            transaction.sign(stellarSdk.Keypair.fromSecret(req.body.receiverSceret));
-            return horizon.submitTransaction(transaction);
-        })
-        .then(function () {
-            return horizon.loadAccount(req.body.issuerPublicKey);
-        })
+    await horizon.loadAccount(issuer.publicKey())
         .then(function (issue) {
-            var newAsset = new stellarSdk.Asset(req.body.assetName, req.body.issuerPublicKey);
-            
-            var transaction = new stellarSdk.TransactionBuilder(issue)
+            console.log(newAssets);
+            let transaction = new stellarSdk.TransactionBuilder(issue)
                 .addOperation(stellarSdk.Operation.payment({
-                    destination: req.body.receiverPublicKey,
-                    asset: newAsset,
-                    amount: req.body.amount
-                }))
-                .build();
-            
-            transaction.sign(stellarSdk.Keypair.fromSecret(req.body.issuerSecret));
-            // return horizon.submitTransaction(transaction);
-            horizon.submitTransaction(transaction).then(function(result){
-                console.log(result);
-                
-            }).catch(function(err){
-                console.log(err);
-            })
+                    destination: distributor.publicKey(),
+                    asset: newAssets,
+                    amount: '10000000'
+                })).build();
+            transaction.sign(issuer);
+            return horizon.submitTransaction(transaction)
+                .then(function (res) {
+                    console.log('Successful Payment of new assets');
+                })
+                .catch(function (err) {
+                    console.error('Failed  Payment of new assets');
+                    error = err.message;
+                    console.error(error);
+                })
         })
         .catch(function (err) {
-            error = err.message;
-            // console.error(err);
-
+            console.error(err);
         })
-    
+
+    // await horizon.loadAccount(distributor.publicKey())
+    //     .then(function (dist) {
+    //         // let newAssets = new stellarSdk.Asset(assetName, issuer.publicKey())
+            
+    //         let transaction = new stellarSdk.TransactionBuilder(dist)
+    //             .addOperation(stellarSdk.Operation.changeTrust({
+    //                 asset: newAssets,
+    //                 limit: limit
+    //             })).build();
+
+    //         transaction.sign(distributor);
+    //         return horizon.submitTransaction(transaction)
+    //         .then(function (res) {
+    //             console.log('Successful Trust Changed of new assets');
+    //             // console.log(res);
+    //         }).catch(function (err) {
+    //             console.error('Failed  Trust Changed of new assets');
+    //             error = err.message;
+    //         })
+
+    //     })
+    //     .then(function () {
+    //         return horizon.loadAccount(issuer.publicKey())
+    //     })
+    //     .then(function (issue) {
+    //         // let newAssets = new stellarSdk.Asset(assetName, issuer.publicKey())
+
+    //         let transaction = new stellarSdk.TransactionBuilder(issue)
+    //             .addOperation(stellarSdk.Operation.payment({
+    //                 destination: distributor.publicKey(),
+    //                 asset: newAssets,
+    //                 amount: amount
+    //             })).build();
+
+    //         transaction.sign(issuer);
+    //         return horizon.submitTransaction(transaction)
+    //             .then(function (res) {
+    //                 console.log('Successful Payment of new assets');
+    //             })
+    //             .catch(function (err) {
+    //                 console.error('Failed  Payment of new assets');
+    //                 error = err.message;
+    //                 console.error(error);
+    //             });
+    //     })
+        // await horizon.loadAccount(issuer.publicKey())
+        // .then(function(issue){
+        //     let transaction = new stellarSdk.TransactionBuilder(issue)
+        //         .addOperation(stellarSdk.Operation.payment({
+        //             destination: distributor.publicKey(),
+        //             asset: newAssets,
+        //             amount: amount
+        //         })).build();
+        //         transaction.sign(issuer);
+        //         return horizon.submitTransaction(transaction)
+        //         .then(function(res) {
+        //             console.log('Successful Payment of new assets');
+        //         })
+        //         .catch(function (err) {  
+        //             console.error('Failed  Payment of new assets');
+        //             error = err.message;
+        //             console.error(error);
+        //         })
+        // })
+        // .catch(function (err) {
+        //     console.error(err);
+        // })
+
     res.render('assets', {
         title: 'Create Assets',
-        error: error
+        error: error,
+        wallet_address: req.body.receiverPublicKey
     })
 });
 
